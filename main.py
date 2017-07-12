e4a107b70c66c3a8918664d13cfb55f03e9a668b
danyahh429 <danyah.harris3@gmail.com>
2017-07-12 16:33:54 -0400

    wrong

--- main.py
+++ main.py
@@ -8,16 +8,12 @@ import os
 from google.appengine.ext import ndb
 
 jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+	loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-              template = jinja_environment.get_template('main.html')
-        self.response.write(template.render()) 
-
-
-class TextBox(ndb.Model):
-	#Txtbox = 
+	def get(self):
+		template = jinja_environment.get_template('main.html')
+	self.response.write(template.render()) 
 
 class ContactsHandler(webapp2.RequestHandler):
 	def get(self):
