2d6ea8f33f70c7ef91aa0e61dd98d60e14961196
unknown <javaughnd.sanders@gmail.com>
2017-07-12 16:23:34 -0400

    html css changes

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ from google.appengine.ext import ndb
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-class TextBox(ndb.Model):
-	Txtbox = 
+##class TextBox(ndb.Model):
+	##Txtbox = 
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-              template = jinja_environment.get_template('main.html')
-        self.response.write(template.render()) 
+	def get(self):
+		template = jinja_environment.get_template('main.html')
+		self.response.write(template.render()) 
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
