bef680d53aa49b57a9ea3441794e0f84db9b8519
danyahh429 <danyah.harris3@gmail.com>
2017-07-12 16:06:47 -0400

    created handlers for each page

--- main.py
+++ main.py
@@ -10,14 +10,38 @@ from google.appengine.ext import ndb
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-class TextBox(ndb.Model):
-	Txtbox = 
-
 class MainHandler(webapp2.RequestHandler):
     def get(self):
               template = jinja_environment.get_template('main.html')
         self.response.write(template.render()) 
 
+
+class TextBox(ndb.Model):
+	#Txtbox = 
+
+class ContactsHandler(webapp2.RequestHandler):
+	def get(self):
+		template = jinja_environment.get_template('contacts.html')
+	self.response.write(template.render())
+
+class ManageHandler(webapp2.RequestHandler):
+	def get(template):
+		template = jinja_environment.get_template('manage.html')
+	self.response.write(template.render())
+
+class HelpHandler(webapp2.RequestHandler):
+	def get(template):
+		template = jinja_environment.get_template('help.html')
+	self.response.write(template.render())
+
+class SettingHandler(webapp2.RequestHandler):
+	def get(template):
+		template = jinja_environment.get_template('settings.html')
+	self.response.write(template.render())
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/contacts', ContactsHandler),
+    ('/help', HelpHandler),
+    ('/settings' SettingHandler),
 ], debug=True)
